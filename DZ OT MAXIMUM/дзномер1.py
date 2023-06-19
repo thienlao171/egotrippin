c82f03d22ff390772530929caf557803378104e3
thienlao171 <136355584+thienlao171@users.noreply.github.com>
2023-06-19 17:25:53 +0300

    Update дзномер1.py

--- DZ OT MAXIMUM/дзномер1.py
+++ DZ OT MAXIMUM/дзномер1.py
@@ -1,8 +1,10 @@
 x = input("введите слово: ")
 def palindromSsss(x):
     return x[::-1] == x
-    while True:
-        if palindromSsss(x):
-            print(f"{x} 'это палиндромом'")
-        else:
-            print('это не палиндромом')
+while True:
+    if palindromSsss(x):
+        print(f"{x} 'это палиндромом'")
+        break
+    else:
+        print('это не палиндромом')
+        break
